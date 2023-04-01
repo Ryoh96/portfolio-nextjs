8a5d5eb6a2f461982be9a61f45afe4610423bcdb
Ryoh96 <account@kurokiryoh.com>
2023-04-01 16:10:00 +0900

    Implemented top page contents

---
+++ src/constants/top-sections.ts
@@ -0,0 +1,18 @@
+export const topSectionTexts = [
+  {
+    title: "RYOH KUROKI",
+    description: "Web Engineering & Design"
+  },
+  {
+    title: "About",
+    description: "経歴・資格・受賞歴"
+  },
+  {
+    title: "Skill",
+    description: "コーディング & デザイン"
+  },
+  {
+    title: "Contact",
+    description: "hello@kurokiryoh.com"
+  }
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <><h1>hello</h1></>
+import type { NextPage } from 'next'
+
+import { topSectionTexts } from '@/constants/top-sections'
+
+const Index: NextPage = () => {
+  return (
+    <>
+      {topSectionTexts.map((section, index) => (
+        <>
+          <h1>{section.title}</h1>
+          <p>{section.description}</p>
+          <button>Show more</button>
+        </>
+      ))}
+    </>
+  )
 }
+
+export default Index
