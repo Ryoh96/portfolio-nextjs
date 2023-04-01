f52083bc16b5b10ff456c01308b487d382c989e8
Ryoh96 <account@kurokiryoh.com>
2023-04-01 15:59:42 +0900

    Implemented base style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { Inter } from 'next/font/google'
-
 export default function Home() {
   return <><h1>hello</h1></>
 }
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components'
+import { Reset } from 'styled-reset'
+
+const Base = createGlobalStyle`
+html {
+  -webkit-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
+
+  body,
+  html {
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    overflow-x:auto;
+    color: #fff;
+    font-family: 'Noto Serif JP', "游明朝体", 'Yu Mincho', 'YuMincho', serif;
+    &::-webkit-scrollbar {
+      /* Chrome, Safari 対応 */
+      display: none;
+    }
+    scrollbar-width: none; /* Firefox 対応 */
+  }
+}
+`
+
+const GlobalStyle = () => {
+  return (
+    <>
+      <Reset />
+      <Base />
+    </>
+  )
+}
+
+export default GlobalStyle
